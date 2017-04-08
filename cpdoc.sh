741066a03846b2dc359bbd06198fbc04d2788b5d
hujiangyi <hujiangyi@dvt.dvt.com>
2017-04-08 22:02:16 +0800

    shell

---
+++ cpdoc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm -rf ../github_doctomd/book/*
+rm -rf ../github_doctomd/md/*
+cp -rf $1/ ../github_doctomd/book/$1
+cd ../github_doctomd/
+./mult-doc-to-md.sh book/
+rm -rf ../github_mydoc/$1
+cp -rf md/$1/ ../github_mydoc/$1
+cd ../github_mydoc
+gitbook build .
+rm -rf ../github_mydoc_pages/*
+cp -rf _book/ ../github_mydoc_pages/
+cd ../github_mydoc_pages/
+git add .
+git commit -a -m "auto commit"
+git fetch
+git rebase
+git push
